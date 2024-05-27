582a59ccae2036154f4ddfe0501c6486385647f1
Hayeong.Lee <bb.kiera@gmail.com>
2024-05-27 13:55:00 +0900

    Update adjust_cost.py

--- neural/wrappers/adjust_cost.py
+++ neural/wrappers/adjust_cost.py
@@ -1,6 +1,5 @@
 import gym
 import numpy as np
-from environment import MazeEnvironmentName, MujocoEnvironmentName
 
 class ActionRelevantCost(gym.Wrapper):
     def __init__(self, env, env_name, cost_weight, cost_lb, eps=1e-5):
@@ -12,21 +11,16 @@ class ActionRelevantCost(gym.Wrapper):
 
     def step(self, action):
         obs, rewards, done, info = super().step(action)
-
-        if isinstance(self._env_name, MazeEnvironmentName):
-            pure_rewards = rewards
-        elif isinstance(self._env_name, MujocoEnvironmentName):
-            # add ctrl_cost
-            if 'half' in self._env_name:
-                ctrl_cost_weight = 0.1
-                healthy_reward = 0.0
-            else: # hopper, walker2d
-                ctrl_cost_weight = 0.001
-                healthy_reward = 1.0
-            ctrl_cost = ctrl_cost_weight * np.sum(action ** 2)
-            pure_rewards = rewards + ctrl_cost - healthy_reward # forward_reward
-        else:
-            raise NotImplementedError
+        
+        # add ctrl_cost
+        if 'half' in self._env_name:
+            ctrl_cost_weight = 0.1
+            healthy_reward = 0.0
+        else: # hopper, walker2d
+            ctrl_cost_weight = 0.001
+            healthy_reward = 1.0
+        ctrl_cost = ctrl_cost_weight * np.sum(action ** 2)
+        pure_rewards = rewards + ctrl_cost - healthy_reward # forward_reward
 
         # set cost func
         info['cost'] = self._cost_weight * np.mean(action ** 2) + self._cost_lb
